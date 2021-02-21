d47d852049cb7eb427936598da84b251aeb82ce7
Jonathan Rodriguez <jrodriguez.5716@gmail.com>
2021-02-21 23:36:34 +0100

    chore(account): Adding watcher saga

--- src/core/accounts/sagas.js
+++ src/core/accounts/sagas.js
@@ -2,6 +2,7 @@ import {
     fork,
     all,
     take,
+    takeEvery,
 } from 'redux-saga/effects';
 
 import actions  from '../../constants/actions';
@@ -10,10 +11,15 @@ import actions  from '../../constants/actions';
 export default function* root() {
     yield all([
         fork(fetchAccounts),
+        fork(watcher),
     ]);
 }
 
-export function* fetchAccounts() {
+function* watcher() {
+    yield takeEvery(actions.GET_ACCOUNTS, fetchAccounts);
+}
+
+function* fetchAccounts() {
     yield take(actions.GET_ACCOUNTS);
 
     console.log('Fetching accounts...');
